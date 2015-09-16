13669f6a543953e105235e96b06f662b3355d77c
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2015-09-16 17:38:37 -0300

    Melhorias na forma de obter os pontos

--- ponto.js
+++ ponto.js
@@ -1,14 +1,20 @@
 function callback() {
-	(function($) {
+	(function ($) {
 		var jQuery = $;
 		// --------------------------------------------------------------------------------
-		function calculaPontosDia(pontos) {
+		function calculaPontosDia(periodoTrabalhado) {
 			var horaAgora = new Date();
 			var agora = horaAgora.getHours() + ":" + horaAgora.getMinutes();
-			pontos[3] = pontos[3] === null ? pontoTextToDateTime([agora])[0] : pontos[3];
-			horasTarde = (pontos[2] === null) ? 0 : (pontos[3] - pontos[2]);
-			horasManha = (pontos[1] === null) ? 0 : (pontos[1] - pontos[0]);
-			var horasTrab = (pontos[0] === null) ? 0 : (horasManha + horasTarde);
+
+			var horasTrab;
+
+			periodoTrabalhado.map(function (pontos) {
+				horasTrab = pontos.reduce(function (a, b) {
+					if (!a) return b;
+					return b - a;
+				});
+			});
+
 			var horasTrabH = (horasTrab / (60 * 60 * 1000));
 			var horasExtrasFolgaH = ((horasTrab - (8.75 * 60 * 60 * 1000)) / (60 * 60 * 1000));
 
@@ -24,44 +30,50 @@ function callback() {
 			}
 		}
 
-		function pontoTextToDateTime(pontos) {
-			return pontos.map(function(b) {
-				if (!b) return null;
-				var d = new Date();
-				var sp = /(\d\d):(\d\d)/.exec(b);
-				d.setUTCHours(sp[1]);
-				d.setUTCMinutes(sp[2]);
-				d.setUTCSeconds(0);
-				d.setUTCMilliseconds(0);
-				return d;
+		function pontoTextToDateTime(periodoTrabalhado) {
+
+			var pontos = periodoTrabalhado.map(function (ponto) {
+				return ponto.split(' ');
+			})
+
+			return pontos.map(function (ponto) {
+				return ponto.map(function (b) {
+					if (!b) return null;
+					var d = new Date();
+					var sp = /(\d\d):(\d\d)/.exec(b);
+					d.setUTCHours(sp[1]);
+					d.setUTCMinutes(sp[2]);
+					d.setUTCSeconds(0);
+					d.setUTCMilliseconds(0);
+					return d;
+				});
 			});
 		}
 
-		window.calcDay = function(pontos){
+		window.calcDay = function (pontos) {
 			return calculaPontosDia(pontoTextToDateTime(pontos));
 		}
 
 
 		function getPontosData() {
-			return $('#diasDoEspelho tr').map(function(a, b) {
-				var pontos = $($(b).children()[3]).text();
-				var reg = /(\d\d:\d\d)? ?(\d\d:\d\d)? ?(\d\d:\d\d)? ?(\d\d:\d\d)? ?/gi;
-				var result = reg.exec(pontos);
-				if (result) result = result.slice(1);
+			return $('#diasDoEspelho tr').map(function (a, b) {
+				var periodoTrabalhado = $($(b).children()[3]).text();
+				var reg = /(\d\d:\d\d) (\d\d:\d\d)/gi;
+				var result = periodoTrabalhado.match(reg);
 				return {
 					dia: $($(b).children()[0]).text(),
-					pontos: result
+					periodoTrabalhado: result
 				};
-			}).toArray().filter(function(a) {
-				if (!a.pontos) {
+			}).toArray().filter(function (a) {
+				if (!a.periodoTrabalhado) {
 					return false
 				};
-				return a.pontos.reduce(function(prev, a) {
+				return a.periodoTrabalhado.reduce(function (prev, a) {
 					return prev || !!a;
 				}, false);
-			}).map(function(a) {
-				a.pontos = pontoTextToDateTime(a.pontos);
-				var result = calculaPontosDia(a.pontos);
+			}).map(function (a) {
+				a.periodoTrabalhado = pontoTextToDateTime(a.periodoTrabalhado);
+				var result = calculaPontosDia(a.periodoTrabalhado);
 				a.horasTrabalhadas = result.horasTrabalhadas;
 				a.horasExtrasFolga = result.horasExtrasFolga;
 				a.isFolga = result.isFolga;
@@ -71,12 +83,12 @@ function callback() {
 
 		var pontosData = getPontosData();
 
-		var espelho = pontosData.map(function(a) {
-			return '</br>+ <strong>Dia</strong>: ' + a.dia + ' <strong>Pontos</strong>: ' + JSON.stringify(a.pontos.map(function(b) {
+		var espelho = pontosData.map(function (a) {
+			return '</br>+ <strong>Dia</strong>: ' + a.dia + ' <strong>Pontos</strong>: ' + JSON.stringify(a.pontos.map(function (b) {
 				if (!b) return null;
 				return b.getUTCHours() + ':' + b.getUTCMinutes();
 			})) + '</br>&nbsp;&nbsp;&nbsp;&nbsp;<strong>Horas Trabalhadas</strong>: ' + a.horasTrabalhadas + '</br>&nbsp;&nbsp;&nbsp;&nbsp;'
-			+'<strong>Horas Extras/Folga</strong>' + ': ' + '<span style="color:' + (a.isFolga ? "red" : "green") + ';">' + a.horasExtrasFolga + '</span>';
+				+ '<strong>Horas Extras/Folga</strong>' + ': ' + '<span style="color:' + (a.isFolga ? "red" : "green") + ';">' + a.horasExtrasFolga + '</span>';
 		}).join('</br>');
 
 		// Pega o tamanho do texto do último campo "Marcações", se menor que 11 então ainda não acabou o dia, tenho que descontar essas horas
@@ -105,9 +117,9 @@ function callback() {
 			}
 
 			var total = saldoAtualEmHoras;
-			
+
 			var hoje = ((dataHoje.getDate() < 10) ? '0' + dataHoje.getDate() : dataHoje.getDate()) + '/' +
-			       ((dataHoje.getMonth()+1 < 10) ? '0' + (dataHoje.getMonth()+1) : (dataHoje.getMonth()+1));
+				((dataHoje.getMonth() + 1 < 10) ? '0' + (dataHoje.getMonth() + 1) : (dataHoje.getMonth() + 1));
 
 			if (ultimoDiaHorasData == hoje) {
 				total = saldoAnteriorEmHoras + saldoAtualEmHoras
@@ -115,7 +127,7 @@ function callback() {
 
 			// Caso apareça o fechamento de horas negativas
 			if (descricaoAtualHoras == "Fechamento BH (-)") {
-				total = saldoAnteriorEmHoras - saldoAtualEmHoras;	
+				total = saldoAnteriorEmHoras - saldoAtualEmHoras;
 			}
 
 			var min = Math.abs(60 * (total - parseInt(total))).toFixed(0)
@@ -132,10 +144,10 @@ function callback() {
 }
 
 
-var removeScripts = function(doc) {
+var removeScripts = function (doc) {
 	var scripts = doc.getElementsByTagName('script');
 	for (var i = scripts.length - 1; i >= 0; i -= 1) {
-		if (typeof(scripts[i].src) === "undefined" || scripts[i].src.indexOf('ponto') === -1) {
+		if (typeof (scripts[i].src) === "undefined" || scripts[i].src.indexOf('ponto') === -1) {
 			scripts[i].nodeValue = "";
 			scripts[i].removeAttribute('src');
 			if (scripts[i].parentNode) {
